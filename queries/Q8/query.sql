356a538652b7b1444ee0be62c3408cec6f1d6aed
Yasin Sancaktutan <sancaktutanyasin@gmail.com>
2021-03-07 10:58:43 +0300

    added q8 query

---
+++ queries/Q8/query.sql
@@ -0,0 +1,7 @@
+select b.primaryTitle ,e.seasonNumber, e.episodeNumber, r.averageRating from `title.ratings` r
+inner join `title.episode` e on r.tconst = e.tconst
+inner join `title.basic` b on b.tconst = e.tconst
+where r.tconst in
+      (select tconst from `title.episode`
+where parentTconst = 'tt0944947')
+order by r.averageRating desc
